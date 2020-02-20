0f613a3a70f6c863d2498b564b72c21617efabf8
Wojt-M <W@192.168.1.20>
2020-02-20 02:26:05 +0100

    commit 4

---
+++ WordSplit/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String s = "Ala ma kota";
+		String[] split1 = s.split(" ");
+
+		int noOfWords = split1.length;
+		System.out.println(noOfWords);		
+		
+	}
+
+}
